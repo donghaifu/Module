fb98e167a8f55df87e5b2907d742615823200eba
fdonghai <fudonghai@yeah.net>
2017-02-13 16:52:51 +0800

    DB

--- TreeView/TreeView/Form1.Designer.cs
+++ TreeView/TreeView/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dgvSalesList = new System.Windows.Forms.DataGridView();
+            this.序号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SalesNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SalesName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.下级名称 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(38, 23);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(346, 558);
+            this.treeView1.TabIndex = 0;
+            // 
+            // dgvSalesList
+            // 
+            this.dgvSalesList.AllowUserToAddRows = false;
+            this.dgvSalesList.AllowUserToDeleteRows = false;
+            this.dgvSalesList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSalesList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSalesList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.序号,
+            this.SalesNo,
+            this.SalesName,
+            this.下级名称});
+            this.dgvSalesList.Location = new System.Drawing.Point(405, 23);
+            this.dgvSalesList.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dgvSalesList.Name = "dgvSalesList";
+            this.dgvSalesList.ReadOnly = true;
+            this.dgvSalesList.RowTemplate.Height = 23;
+            this.dgvSalesList.Size = new System.Drawing.Size(809, 566);
+            this.dgvSalesList.TabIndex = 20;
+            // 
+            // 序号
+            // 
+            this.序号.DataPropertyName = "No";
+            this.序号.HeaderText = "序号";
+            this.序号.Name = "序号";
+            this.序号.ReadOnly = true;
+            // 
+            // SalesNo
+            // 
+            this.SalesNo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SalesNo.DataPropertyName = "AssembleNo";
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SalesNo.DefaultCellStyle = dataGridViewCellStyle3;
+            this.SalesNo.HeaderText = "上级";
+            this.SalesNo.Name = "SalesNo";
+            this.SalesNo.ReadOnly = true;
+            this.SalesNo.Width = 62;
+            // 
+            // SalesName
+            // 
+            this.SalesName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SalesName.DataPropertyName = "NextLevel";
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SalesName.DefaultCellStyle = dataGridViewCellStyle4;
+            this.SalesName.HeaderText = "下级";
+            this.SalesName.Name = "SalesName";
+            this.SalesName.ReadOnly = true;
+            this.SalesName.Width = 62;
+            // 
+            // 下级名称
+            // 
+            this.下级名称.DataPropertyName = "NextLevelName";
+            this.下级名称.HeaderText = "下级名称";
+            this.下级名称.Name = "下级名称";
+            this.下级名称.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 615);
+            this.Controls.Add(this.dgvSalesList);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesList)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dgvSalesList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 序号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SalesNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SalesName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 下级名称;
     }
 }
 
--- TreeView/TreeView/Form1.cs
+++ TreeView/TreeView/Form1.cs
@@ -6,14 +6,95 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TreeView
 {
     public partial class Form1 : Form
     {
+        DataSet ds = new DataSet();
+        //ds.Tables["SalesList"];
         public Form1()
         {
             InitializeComponent();
+            getSaleList();
         }
+
+        private void getSaleList()
+        {
+            SqlConnection cn = new SqlConnection(Properties.Settings.Default.HarvestConnectionString);
+            string sql = " SELECT No,AssembleNo,NextLevel,NextLevelName FROM TempTable WHERE SalesNo ='10001' ORDER BY LEVEL";
+            //where  AssembleNo LIKE '__0__' AND LevelHigh = '是' ORDER BY AssembleNo 
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+
+            cn.Open();
+            da.Fill(ds, "SalesList");
+            cn.Close();
+            dgvSalesList.DataSource = ds.Tables["SalesList"];
+            //treeView1.Nodes
+            TreeNodeCollection tnc = treeView1.Nodes;
+            BindTreeViewN(ds.Tables["SalesList"], tnc, "10001", "NextLevel", "AssembleNo", "NextLevelName");
+            //BindTreeView(ds.Tables["SalesList"], tnc, 1, "No" ,"NextLevel", "AssembleNo", "NextLevelName");
+        }
+
+        /// <summary>
+        /// 绑定TreeView（利用TreeNodeCollection）,把“没有规律”的链接关系从数据库中取出来后自己进行递归排序
+        /// </summary>
+        /// <param name="tnc">TreeNodeCollection（TreeView的节点集合）</param>
+        /// <param name="pid_val">父id的字段值,最初输入的值是树根的值，比如10001</param>
+        /// <param name="id">数据库 id 字段名</param>
+        /// <param name="pid">数据库 父id 字段名</param>
+        /// <param name="text">数据库 文本 字段值</param>
+        private void BindTreeViewN(DataTable dt, TreeNodeCollection tnc, string pid_val, string id, string pid, string text)
+        {
+            DataView dv = new DataView(dt);//将DataTable存到DataView中，以便于筛选数据
+            TreeNode tn;//建立TreeView的节点（TreeNode），以便将取出的数据添加到节点中
+            //以下为三元运算符，如果父id为空，则为构建“父id字段 is null”的查询条件，否则构建“父id字段=父id字段值”的查询条件
+            string filter = string.IsNullOrEmpty(pid_val) ? pid + " is null" : string.Format(pid + "='{0}'", pid_val);
+            dv.RowFilter = filter;//利用DataView将数据进行筛选，选出相同 父id值 的数据
+            foreach (DataRowView drv in dv)
+            {
+                tn = new TreeNode();//建立一个新节点（学名叫：一个实例）
+                tn.Name = drv[id].ToString();//节点的Value值，一般为数据库的id值
+                tn.Text = drv[id].ToString() + drv[text].ToString();//节点的Text，节点的文本显示，修改了一下把结点的id也加了进去
+                tnc.Add(tn);//将该节点加入到TreeNodeCollection（节点集合）中
+                BindTreeViewN(dt, tn.Nodes, tn.Name, id, pid, text);//递归（反复调用这个方法，直到把数据取完为止）
+            }
+        }
+
+        /// <summary>
+        /// 绑定TreeView（利用TreeNodeCollection）,把“有规律”的链接关系从数据库中取出来后自己进行递归排序
+        /// </summary>
+        /// <param name="tnc">TreeNodeCollection（TreeView的节点集合）</param>
+        /// <param name="no">数据库 no 字段名,表示层级关系</param>
+        /// <param name="pid_val">父id的字段值,最初输入的值是树根的值，比如10001</param>
+        /// <param name="id">数据库 id 字段名</param>
+        /// <param name="pid">数据库 父id 字段名</param>
+        /// <param name="text">数据库 文本 字段值</param>
+        private void BindTreeView(DataTable dt, TreeNodeCollection tnc, int No_val, string No, string id, string pid, string text)
+        {
+            DataView dv = new DataView(dt);//将DataTable存到DataView中，以便于筛选数据
+            TreeNode tn;//建立TreeView的节点（TreeNode），以便将取出的数据添加到节点中
+            //以下为三元运算符，如果父id为空，则为构建“父id字段 is null”的查询条件，否则构建“父id字段=父id字段值”的查询条件
+            //string filter = string.IsNullOrEmpty(No_val) ? No + " is null" : string.Format(No + "='{0}'", No_val);
+            //if(No_val)
+            string filter = string.Format(No + "='{0}'", No_val);
+     
+            No_val ++;
+            dv.RowFilter = filter;//利用DataView将数据进行筛选，选出相同 层级 的数据
+            foreach (DataRowView drv in dv)
+            {
+                tn = new TreeNode();//建立一个新节点（学名叫：一个实例）
+                tn.Name = drv[id].ToString();//节点的Value值，一般为数据库的id值
+                tn.Text = drv[id].ToString() + drv[text].ToString();//节点的Text，节点的文本显示，修改了一下把结点的id也加了进去
+                tnc.Add(tn);//将该节点加入到TreeNodeCollection（节点集合）中
+
+                BindTreeView(dt, tn.Nodes, No_val, No, id, pid, text);//递归（反复调用这个方法，直到把数据取完为止）
+            }
+        }
+
+
+
+
     }
 }
