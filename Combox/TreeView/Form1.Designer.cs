fa0ee0eeb7f1899b2d7bf9325eba39f46e6381f0
fdonghai <fudonghai@yeah.net>
2017-02-16 16:54:08 +0800

    combobox实现件号和汉字双重模糊查询

---
+++ Combox/TreeView/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle22 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvSalesList = new System.Windows.Forms.DataGridView();
+            this.序号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SalesNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SalesName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.下级名称 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSalesList
+            // 
+            this.dgvSalesList.AllowUserToAddRows = false;
+            this.dgvSalesList.AllowUserToDeleteRows = false;
+            this.dgvSalesList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSalesList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSalesList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.序号,
+            this.SalesNo,
+            this.SalesName,
+            this.下级名称});
+            this.dgvSalesList.Location = new System.Drawing.Point(231, 23);
+            this.dgvSalesList.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dgvSalesList.Name = "dgvSalesList";
+            this.dgvSalesList.ReadOnly = true;
+            this.dgvSalesList.RowTemplate.Height = 23;
+            this.dgvSalesList.Size = new System.Drawing.Size(741, 566);
+            this.dgvSalesList.TabIndex = 20;
+            // 
+            // 序号
+            // 
+            this.序号.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.序号.DataPropertyName = "No";
+            this.序号.HeaderText = "序号";
+            this.序号.Name = "序号";
+            this.序号.ReadOnly = true;
+            this.序号.Width = 62;
+            // 
+            // SalesNo
+            // 
+            this.SalesNo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SalesNo.DataPropertyName = "AssembleNo";
+            dataGridViewCellStyle21.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SalesNo.DefaultCellStyle = dataGridViewCellStyle21;
+            this.SalesNo.HeaderText = "上级";
+            this.SalesNo.Name = "SalesNo";
+            this.SalesNo.ReadOnly = true;
+            this.SalesNo.Width = 62;
+            // 
+            // SalesName
+            // 
+            this.SalesName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.SalesName.DataPropertyName = "NextLevel";
+            dataGridViewCellStyle22.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SalesName.DefaultCellStyle = dataGridViewCellStyle22;
+            this.SalesName.HeaderText = "下级";
+            this.SalesName.Name = "SalesName";
+            this.SalesName.ReadOnly = true;
+            this.SalesName.Width = 62;
+            // 
+            // 下级名称
+            // 
+            this.下级名称.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.下级名称.DataPropertyName = "NextLevelName";
+            this.下级名称.HeaderText = "下级名称";
+            this.下级名称.Name = "下级名称";
+            this.下级名称.ReadOnly = true;
+            this.下级名称.Width = 92;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(33, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 23);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 25);
+            this.textBox1.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 615);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgvSalesList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSalesList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSalesList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 序号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SalesNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SalesName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 下级名称;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Combox/TreeView/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TreeView
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+        //初始化绑定默认关键词，此数据源可以从数据库取
+        List<string> listOnit = new List<string>();
+        //输入key之后，返回的关键词
+        List<string> listNew = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //得到数据
+            getSaleList();
+            //绑定到固定的列表和下面二选一
+            //BindComboBoxToList();
+            //绑定到数据源
+            BindComboBoxToDataTable();
+        }
+
+
+        private void getSaleList()
+        {
+            SqlConnection cn = new SqlConnection(Properties.Settings.Default.HarvestConnectionString);
+            string sql = " SELECT No,AssembleNo,NextLevel,NextLevelName,Remark FROM TempTable WHERE SalesNo ='10001' ";
+            //where  AssembleNo LIKE '__0__' AND LevelHigh = '是' ORDER BY AssembleNo 
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+
+            cn.Open();
+            da.Fill(ds, "SalesList");
+            cn.Close();
+            dgvSalesList.DataSource = ds.Tables["SalesList"];
+            //LeftMatch();
+        }
+
+        //从数据源中选择一列做输入匹配，使用combobox自己的匹配功能，只能从左到右进行匹配，不能模糊查询
+        private void LeftMatch()
+        {
+            AutoCompleteStringCollection stringCol = new AutoCompleteStringCollection();
+            foreach (DataRow row in ds.Tables["SalesList"].Rows)
+            {
+                //取第2列数据，从0开始
+                stringCol.Add(Convert.ToString(row[2]));
+            }
+            comboBox1.AutoCompleteCustomSource = stringCol;        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //comboBox1.AutoCompleteCustomSource = ds.Tables["SalesList"].Rows;
+            textBox1.Text = comboBox1.Text;
+        }
+
+        //绑定列表
+        private void BindComboBoxToList()
+        {
+            listOnit.Add("张三");
+            listOnit.Add("三张");
+            listOnit.Add("冯张三");
+            listOnit.Add("马六");
+            listOnit.Add("孙楠");
+            listOnit.Add("戚六马");
+            listOnit.Add("刘欢");
+            listOnit.Add("刘欢欢");
+            //注意使用Item.Add(obj)或者Item.AddRange(obj)方式添加
+            //如果用DataSource绑定，后面再进行绑定是不行的，即便是Add或者clear也不行
+            this.comboBox1.Items.AddRange(listOnit.ToArray());
+        }
+
+        //绑定数据源
+        private void BindComboBoxToDataTable()
+        {
+
+            //去除重复的字段,先筛选"NextLevel"字段
+            string[] str = new string[] { "NextLevel"};
+            //将DataTable转成DataView
+            DataView dw = ds.Tables["SalesList"].DefaultView;
+            //先建立一个DataTable
+            DataTable DtForBind = dw.ToTable(true,str);
+            
+            foreach (DataRow row in DtForBind.Rows)
+            {
+                //取第2列数据，从0开始
+                listOnit.Add(Convert.ToString(row[0]));
+            }
+
+            //去除重复的字段,再筛选"NextLevelName"字段
+            string[] str1 = new string[] { "NextLevelName"};
+            DtForBind = dw.ToTable(true, str1);
+            foreach (DataRow row in DtForBind.Rows)
+            {
+                //取第2列数据，从0开始
+                listOnit.Add(Convert.ToString(row[0]));
+            }
+
+            //最后的listOnit是两个字段的合体，完成件号和汉字双重检索
+
+            //有时间验证一下count数量是否对得上
+            this.comboBox1.Items.AddRange(listOnit.ToArray());
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            //清空combobox
+            this.comboBox1.Items.Clear();
+            //清空ListNew
+            listNew.Clear();
+            //遍历全部备查数据
+            foreach (var item in listOnit)
+            {
+                if (item.Contains(this.comboBox1.Text))
+                {
+                    listNew.Add(item);
+                }
+            }
+            //combobox添加已经查到的关键词
+            this.comboBox1.Items.AddRange(listNew.ToArray());
+            //设置光标位置，否则光标位置始终保持在第一列，造成输入的关键词倒序排列
+            this.comboBox1.SelectionStart = this.comboBox1.Text.Length;
+            //保持鼠标指针形状，有时鼠标指针会被覆盖，所以要进行一次设置
+            Cursor = Cursors.Default;
+            //自动弹出下拉框
+            this.comboBox1.DroppedDown = true;
+
+        }
+
+    }
+}
